b4c3f7f8113b72fab8d36319d66fde1480552be2
terasum <terasum@163.com>
2018-05-23 09:03:34 +0800

    Update dependencies

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,8 +4,13 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+import Antd from 'vue-antd-ui'
+import 'vue-antd-ui/dist/antd.css'
+
+Vue.use(Antd)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
