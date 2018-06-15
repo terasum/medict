83a957bb93418a54b194d12aa9175c654f5aa3f1
terasum <terasum@163.com>
2018-06-15 21:25:40 +0800

    feat(*): add new interface, and finish the query word (stage-02)

--- src/renderer/components/MainWindow2.vue
+++ src/renderer/components/MainWindow2.vue
@@ -5,12 +5,12 @@
             <Header>
                <div class="header-content">
                    <div class="header-logo header-item"></div>
-                   <Input class="header-item" :value="word" @input="searchWord" icon="search" placeholder="Enter something..." style="width: 200px"/>
+                   <Input class="header-item" :value="word" @keyup.enter.native="searchWord($event)" icon="search" placeholder="Enter something..." style="width: 200px"/>
                    <ButtonGroup>
                      <Button icon="arrow-left-b"></Button>
                      <Button icon="arrow-right-b"></Button>
                    </ButtonGroup>
-                   <Select v-model="model1" style="width:200px">
+                   <Select :value.sync="dictName" style="width:200px">
                      <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
                    </Select>
                    <Dropdown trigger="click">
@@ -25,12 +25,13 @@
             </Header>
             <Layout :style="{ overflow: 'hidden', background: '#fff', height: '100%'}">
                 <Sider hide-trigger :style="{ overflow: 'hidden', background: '#fff', height: '100%'}">
-                    
                 </Sider>
                 <Layout>
-                    <Content class="dict-content">
+                    <Content class="dict-content" >
+                      <div class="defination-content" v-html="content">
+                      </div>
                         <!-- {{cwdpath}}  -->
-                        {{content}}
+                        <!-- {{content}} -->
                     </Content>
                 </Layout>
             </Layout>
@@ -144,19 +145,56 @@ padding: '0 2px' ;
         // 当前搜索解释
         content () {
           return this.$store.state.medict.content
+        },
+        dictName () {
+          console.log('aaa.')
+          console.log(this.$store.state.medict.dict)
+          return this.$store.state.medict.dict.name
         }
       },
       methods: {
-        searchWord (word) {
-          console.log(word)
-          console.log(this)
-          this.$store.dispatch('init')
-          this.$store.dispatch('search', word)
+        // searchWord when input the word in input box
+        searchWord (ev) {
+          if (ev.target === undefined || ev.target.value === undefined || ev.target.value.trim() === '') {
+            console.log(ev.target.value)
+            return
+          }
+          let word = ev.target.value
+          console.log(word, this.$store.state.medict.searchWord)
+          if (word === this.$store.state.medict.searchWord) {
+            console.log('same word...')
+            return
+          }
+          if (!this.$store.state.medict.dict.inited) {
+            this.$Message.info('loading dict, please waiting...')
+            return
+          }
+          // console.log(this)
+          // this.$store.dispatch('init')
+          if (word.trim() !== '') {
+            this.$store.dispatch('search', word)
+          }
+        },
+        // select a dict in selection box
+        selectDict (e) {
+          console.log(e)
         }
       },
       watch: {
         // 观察变化
         // myvalue: (newVal, oldVal)=>  { }
+        dictName: function (newVal, oldVal) {
+          console.log(newVal, oldVal)
+          console.log(this.$refs)
+        }
+      },
+      mounted () {
+        setTimeout(() => {
+          // 需要用 this.$nextTick 以确保在下次更新之后开始初始化
+          this.$nextTick(() => {
+            this.$store.dispatch('init')
+          })
+        }, 100)
       }
     }
 </script>
