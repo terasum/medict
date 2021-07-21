05e57312c5402251f383a61555474c2a2203cb47
terasum <terasum@163.com>
2021-07-22 00:01:49 +0800

    chore(.github): support create .env file

--- tests/apis/baidu_translate.api.key.ts
+++ tests/apis/baidu_translate.api.key.ts
@@ -1,5 +1,3 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-console.log('Now the value for FOO is:', process.env.BAIDU_APPID);
-console.log('Now the value for FOO is:', process.env.BAIDU_APP_KEY);
