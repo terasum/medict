f9ffbf7aa7d7377e89d33a1f4cd33032a98dcafa
terasum <terasum@163.com>
2021-07-24 20:25:20 +0800

    test(apis): fix baidu translate testcase

--- tests/apis/baidu_translate.spec.ts
+++ tests/apis/baidu_translate.spec.ts
@@ -1,14 +1,28 @@
 import { translate } from '../../src/apis/baidu_translate';
 import './baidu_translate.api.key';
-
+import mockAxios from 'jest-mock-axios';
 import { assert } from 'chai';
 
 const appid = process.env.BAIDU_APPID || '';
 const appkey = process.env.BAIDU_APP_KEY || '';
 
+afterEach(() => {
+  // cleaning up the mess left behind the previous test
+  mockAxios.reset();
+});
+
+
 describe('百度翻译测试', () => {
   it('英文->中文', () => {
-    return translate(appid, appkey, 'en', 'zh', 'apple').then(resp => {
+    let testdata = {data: {
+      from: 'en',
+      to: 'zh',
+      trans_result: [{ src: 'apple', dst: '苹果' }],
+    }};
+    const promise = translate(appid, appkey, 'en', 'zh', 'apple');
+    mockAxios.mockResponse(testdata);
+
+    return promise.then(resp => {
       assert.deepEqual(resp.status, 200);
       assert.deepEqual(resp.data, {
         from: 'en',
@@ -17,8 +31,19 @@ describe('百度翻译测试', () => {
       });
     });
   });
+
   it('中文->英文', () => {
-    return translate(appid, appkey, 'zh', 'en', '这是一本词典').then(resp => {
+    
+    let testdata = {data: {
+        from: 'zh',
+        to: 'en',
+        trans_result: [{ src: '这是一本词典', dst: 'This is a dictionary' }],
+      }};
+
+    const promise = translate(appid, appkey, 'zh', 'en', '这是一本词典');
+    mockAxios.mockResponse(testdata);
+
+    return promise.then(resp => {
       assert.deepEqual(resp.status, 200);
       assert.deepEqual(resp.data, {
         from: 'zh',
