4d8aa78ba0720eca75521ad5759a89c2a4a66e79
terasum <terasum@163.com>
2021-08-11 17:18:30 +0800

    feat(ui): enhance traslate ui interface

--- src/renderer/view/PluginsWindow.vue
+++ src/renderer/view/PluginsWindow.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid" style="height: 100%">
     <Header :displaySearchBox="true" />
-    <div class="row" style="height: 100%">Plugins</div>
+    <div class="fake-plugin" style="height: 100%"><span>Plugins</span></div>
     <FooterBar />
   </div>
 </template>
@@ -17,4 +17,20 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+.fake-plugin{
+  background:#fafafa;
+  padding:0;
+  margin:0 -12px;
+  
+  span{
+    color: #fafafa;  /*文字颜色必须和背景同色*/
+    font-size: 64px;
+    margin: auto;
+    display: block;
+    text-align: center;
+    padding-top: 140px;
+    text-shadow: 1px 1px 1px rgb(109, 107, 107),-1px -1px 1px #fff;
+    user-select: none;
+  }
+}
 </style>
