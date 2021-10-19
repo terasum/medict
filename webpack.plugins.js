5bb1b171311871bd36f44c21a1b80d181159b878
terasum <terasum@163.com>
2021-10-19 19:33:21 +0800

    feat(DocsWinow.vue): add user manual and docs window

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,14 +1,26 @@
+const path = require('path');
+
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const assets = ['docs', 'css']; // asset directories
 
-module.exports = [
+const plugins = [
   new ForkTsCheckerWebpackPlugin(),
   new VueLoaderPlugin(),
   new MiniCssExtractPlugin({
-      filename:"css~[name]-[chunkhash:12].css"
+    filename: 'css~[name]-[chunkhash:12].css',
+  }),
+  new CopyWebpackPlugin({
+    patterns: assets.map((asset) => {
+      return {
+        from: path.resolve(__dirname, 'src/renderer/assets', asset),
+        to: path.resolve(__dirname, '.webpack/renderer/assets', asset),
+      };
+    }),
   }),
   new BundleAnalyzerPlugin({
     analyzerMode: 'static',
@@ -16,3 +28,5 @@ module.exports = [
     openAnalyzer: false,
   }),
 ];
+
+module.exports = plugins;
